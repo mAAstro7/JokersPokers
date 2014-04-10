9cc4fa119746e3e3b4ac06881f46cd438fad0843
Joonatan Vuorela <joonatan.vuorela@cs.helsinki.fi>
2014-04-10 13:00:13 +0300

    pit-raportti upload

--- JokersPokers/src/main/java/JokersPokers/JokersPokers/Ohjelma.java
+++ JokersPokers/src/main/java/JokersPokers/JokersPokers/Ohjelma.java
@@ -11,17 +11,6 @@ public class Ohjelma {
 
     
     public static void main(String[] args) {
-        Kortti kortti1 = new Kortti("pata", 2);
-        Kortti kortti2 = new Kortti("pata", 3);
-        Kortti kortti3 = new Kortti("pata", 4);
-        Kortti kortti4 = new Kortti("pata", 5);
-        Kortti kortti5 = new Kortti("ruutu", 9);
-        Pokerikasi kasi = new Pokerikasi(kortti1, kortti2, kortti3, kortti4, kortti5);
-        KadenTunnistaja tunnistaja = new KadenTunnistaja();
-        if (tunnistaja.onkoHai(kasi)) {
-            System.out.println("ON KÄSIII");
-        } else {
-            System.out.println("tänne ei päästä");
-        }
+
     }
 }
