e08ef0b5bea30158804e9ae518300ad43aabf454
vujo <joonatan.vuorela@cs.helsinki.fi>
2014-04-25 10:44:52 +0300

    upload

--- JokersPokers/src/main/java/JokersPokers/JokersPokers/Ohjelma.java
+++ JokersPokers/src/main/java/JokersPokers/JokersPokers/Ohjelma.java
@@ -1,13 +1,7 @@
 package JokersPokers.JokersPokers;
 
 import JokersPokers.Kayttoliittyma.Kayttoliittyma;
-import JokersPokers.Kortti.Kortti;
-import JokersPokers.Pelaaja.Pokerikasi;
-import JokersPokers.Kortti.Korttipakka;
-import JokersPokers.Pelaaja.Pelivaraukset;
-import JokersPokers.Pokerikadet.*;
-import JokersPokers.Tunnistaja.KadenTunnistaja;
-import javax.swing.SwingUtilities;
+import javax.swing.*;
 
 public class Ohjelma {
 
